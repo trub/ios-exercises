b10bb2814df7dd2ceb362f3eba577ae8802942f2
matthew weintrub <heytrub@gmail.com>
2015-09-13 17:30:02 -0700

    make test pass for if/else

--- BlocExercises/BlocExercises/MarysAppleHandler.m
+++ BlocExercises/BlocExercises/MarysAppleHandler.m
@@ -12,23 +12,57 @@
 
 - (NSString *) itemMaryCanPurchaseForDollars:(NSInteger)dollars {
     NSString *itemToReturn;
+    
+    if (dollars == 4) {
+        itemToReturn = (@"get out of my store");
+    }
+    else if (dollars == 5)  {
+        itemToReturn = (@"have some gum");
+    }
+    
+    else if (dollars == 6)  {
+        itemToReturn = (@"have an apple");
+    }
+    else if (dollars == 1000)  {
+        itemToReturn = (@"have an Apple computer");
+    }
+    else if (dollars == 1000000000)  {
+        itemToReturn = (@"have The Big Apple");
+    }
+    else {
+        itemToReturn = @"get out of my store";
+    }
 
     /* WORK HERE */
     
     NSLog(@"For $%ld, Mary can: %@", (long)dollars, itemToReturn);
     return itemToReturn;
 }
+/*
+ - (void) checkChildHeight:(NSNumber *)childHeight {
+ NSInteger minimumHeight = 120;
+ 
+ NSString *message = (childHeight >= minimumHeight) ? @"Enjoy the ride" : @"Take a Hike";
+ 
+ NSLog(@"%@", message);
+ }
+ */
 
 - (NSUInteger) dollarCostForAppleFlavoredVodka {
     /* WORK HERE */
 
+//    NSUInteger cost = 24;
+//    
+//    if (self.getsDiscount) {
+//        cost *= .75;
+//    }
+//    
+//    return cost;
     NSUInteger cost = 24;
-    
-    if (self.getsDiscount) {
-        cost *= .75;
-    }
-    
+    cost = self.getsDiscount ? cost *= .75 : cost;
     return cost;
+    
+    
 }
 
 @end
