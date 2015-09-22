de6fd857c3bf7df7a539c636f8e82b69db3b6ffc
matthew weintrub <heytrub@gmail.com>
2015-09-21 20:56:24 -0500

    makes 1st and 3rd tests pass

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -12,16 +12,23 @@
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
     /* WORK HERE */
-    return @0;
+    NSInteger num = [number integerValue] * 2;
+    return [NSNumber numberWithInt:num];
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
+    /* NSMutableArray *mutableArrayofNumbersBetweenNumber = [[NSMutableArray alloc] init]; */
+
     return @[];
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
     /* WORK HERE */
+    NSMutableArray *mutableArrayOfNumbers = [arrayOfNumbers mutableCopy];
+    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:nil ascending:YES];
+    [mutableArrayOfNumbers sortUsingDescriptors:@[sortDescriptor]];
+    return [mutableArrayOfNumbers[0] integerValue];
     return 0;
 }
 
